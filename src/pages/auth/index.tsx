743829b9bc3c70b8418b6d3b397244ae7e40b03e
Eduardo Larios <elariosespinoza2@gmail.com>
2025-09-15 17:21:00 -0600

    Auth with Google tested

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,7 +1,27 @@
+import { auth, provider } from "../../config/firebase";
+import { signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
 function Auth() {
+  const navigate = useNavigate();
+
+  const authWithGoogle = async () => {
+    const result = await signInWithPopup(auth, provider);
+    const authInfo = {
+      userID: result.user.uid,
+      name: result.user.displayName,
+      profilePhoto: result.user.photoURL,
+      isAuth: true,
+    };
+
+    localStorage.setItem("auth", JSON.stringify(authInfo));
+  };
+
   return (
-    <div>Auth</div>
-  )
+    <div>
+      <button onClick={authWithGoogle}>Google</button>
+    </div>
+  );
 }
 
-export default Auth
+export default Auth;
